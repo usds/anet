ab8a96cd8a86399825a042daeeada159130a70c7
José García <jose@west.nl>
2020-01-02 11:39:35 +0100

    Expand Kerberos unit test

--- src/integration-test/java/mil/dds/anet/integrationtest/auth/KerberosClientTest.java
+++ src/integration-test/java/mil/dds/anet/integrationtest/auth/KerberosClientTest.java
@@ -3,7 +3,10 @@ package mil.dds.anet.integrationtest.auth;
 import static org.assertj.core.api.Assertions.assertThat;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Base64;
+import org.ietf.jgss.GSSCredential;
 import org.junit.Test;
 import org.pac4j.core.context.HttpConstants;
 import org.pac4j.core.profile.CommonProfile;
@@ -21,14 +24,40 @@ public class KerberosClientTest {
 
   @Test
   public void test() throws IOException {
+    /*
     SunJaasKerberosTicketValidator validator = new SunJaasKerberosTicketValidator();
     // HTTP/fully-qualified-domain-name@DOMAIN
     validator.setServicePrincipal("jose/admin@LOCALHOST");
     // the keytab file must contain the keys for the service principal, and should be protected
     validator.setKeyTabLocation(new FileSystemResource("/tmp/krb5cc_1001"));
     // validator.setDebug(true);
+    */
+
+
+    SunJaasKerberosTicketValidator validator = new SunJaasKerberosTicketValidator();
+    // HTTP/fully-qualified-domain-name@DOMAIN
+    validator.setServicePrincipal("user4" + "@LOCALHOST");
+    // the keytab file must contain the keys for the service principal, and should be protected
+    validator.setKeyTabLocation(new FileSystemResource("/home/jose/Downloads/tmp2/http.localhost.keytab"));
+    validator.setDebug(true);
+
+    byte[] fileContent = new String(Files.readAllBytes(Paths.get("/tmp/krb5cc_1001"))).getBytes(StandardCharsets.UTF_8);
+    //assertThat(fileContent).isEqualTo("");
+
+    byte[] KERBEROS_TICKET = Base64.getEncoder().encode(fileContent);
+    // byte[] KERBEROS_TICKET = Files.readAllBytes(Paths.get("/tmp/krb5cc_1001"));
+    //validator.validateTicket(KERBEROS_TICKET).username();
 
     final DirectKerberosClient client = new DirectKerberosClient(new KerberosAuthenticator(validator));
+
+    String username = validator.validateTicket(fileContent).username();
+    
+    assertThat(username).isEqualTo("");
+    
+    KerberosCredentials kc = new KerberosCredentials(KERBEROS_TICKET);
+    assertThat(kc.getUserProfile()).isNotNull();
+    assertThat(client.getUserProfile(kc, null)).isNotNull();
+
     final MockWebContext context = MockWebContext.create();
 
     context.addRequestHeader(HttpConstants.AUTHORIZATION_HEADER, "Negotiate " + new String(KERBEROS_TICKET, StandardCharsets.UTF_8));
